ec1f48c8cf0b69a85ca4e0971aa75f44c8bd7868
Andrew Y. Chen <65506481+chenandrewy@users.noreply.github.com>
2022-08-19 14:32:22 -0400

    safety save

--- 2_figures_simple.R
+++ 2_figures_simple.R
@@ -13,15 +13,15 @@ library(nlme)
 
 SUBDIR = 'Full Sets OP'; FILENAME = 'PredictorPortsFull.csv'
 
-dir.create('./results/')
+dir.create('../results/')
 
 # Generate McLean-Pontiff bootstrapped mean returns figure ====
 # read in PredictorPortsFull.csv
-ret0 = fread('data/PredictorPortsFull.csv') %>%                                           
+ret0 = fread('../data/PredictorPortsFull.csv') %>%                                           
   filter(!is.na(ret), port == 'LS')
 
 # read in SignalDoc.csv and isolates to relevant date cols
-signaldoc = fread('data/SignalDoc.csv') %>% 
+signaldoc = fread('../data/SignalDoc.csv') %>% 
   mutate(
     signalname = Acronym
     , pubdate = as.Date(paste0(Year, '-12-31'))
@@ -60,8 +60,9 @@ bootfun = function(sampname){
   # make wide dataset, use NA if not correct sample
   wide_is = ret1 %>%
     filter(samptype == sampname) %>% 
+    select(c(date, "ret", signalname)) %>% 
     pivot_wider(
-      c(date, "ret", signalname), names_from = "signalname", values_from = "ret"
+      names_from = "signalname", values_from = "ret"
     ) %>% 
     select(-date) %>% 
     as.matrix()
@@ -86,6 +87,7 @@ bootfun = function(sampname){
 # bootstrap for each sample type
 rboot1 = bootfun('in-samp')
 rboot2 = bootfun('out-of-samp')
+rboot3 = bootfun('post-pub')
 
 # compile and plot
 bootdat = data.frame(
@@ -94,6 +96,11 @@ bootdat = data.frame(
   rbind(
     data.frame(
       pooled_mean_ret = rboot2, samptype = 'out-of-samp' 
+    )
+  ) %>% 
+  rbind(
+    data.frame(
+      pooled_mean_ret = rboot3, samptype = 'post-pub' 
     )
   )
 
@@ -109,11 +116,32 @@ bootdat  %>%
 
 
 ggsave(
-  "results/MPrep.pdf",
+  "../results/MPrep.pdf",
   width = 12,
   height = 12
 )
 
+
+
+## simple calc ====
+
+ret1 %>% 
+  group_by(signalname, samptype) %>% 
+  summarize(rbar = mean(ret)) %>% 
+  group_by(samptype) %>% 
+  summarize(mean(rbar))
+
+
+bootdat %>% 
+  group_by(samptype) %>% 
+  summarize(
+    pooled_mean = mean(pooled_mean_ret), se = sd(pooled_mean_ret)
+  ) %>% 
+  mutate(
+    mean_scaled = pooled_mean / 0.667 *100
+    , se_scaled = se / 0.667 * 100
+    , decay_scaled = 100 - mean_scaled
+  )
 
 
 # Generate R2 replication figure ====
@@ -381,7 +409,7 @@ print(xtable(tab_too_big_wide, type = "latex"), file = "../results/tab-too-big.t
 
 # Correlation Figures ------------------------------------------------------
 
-# import data
+# import dat
 doc =  fread("../data/SignalDoc.csv") %>%
   rename(signalname = Acronym) %>%
   select(-c(`Detailed Definition`, `Notes`))
@@ -433,3 +461,108 @@ ggplot(plotme, aes(x=Num_PC, y = pct_explained)) + geom_line() +
   )
 
 
+
+# Filling the Gap ---------------------------------------------------------
+
+
+# import dat
+doc =  fread("../data/SignalDoc.csv") %>%
+  rename(signalname = Acronym) %>%
+  select(-c(`Detailed Definition`, `Notes`))
+
+
+cz_all = fread(paste0("../data/PredictorPortsFull.csv"))  %>% 
+  select(signalname, port, date, ret) %>%
+  left_join(
+    doc %>% select(signalname, SampleStartYear, SampleEndYear)
+    , by = 'signalname'
+  ) %>%
+  mutate(
+    insamp = (year(date) >= SampleStartYear) &  (year(date) <= SampleEndYear)
+  )
+
+cz_sum = cz_all %>% group_by(signalname) %>% filter(insamp, port == 'LS') %>% 
+  summarize(tstat = mean(ret)/sd(ret)*sqrt(n()))
+
+
+sigmu = 2.5
+tmod = abs(rnorm(1e4, 0, sqrt(sigmu^2 + 1)))
+
+plotme = data.table(
+  group = 'emp', tstat = cz_sum$tstat
+) %>% rbind(
+  data.table(
+    group = 'mod', tstat = tmod[tmod > 2]
+  ) 
+)
+  
+  
+ggplot(plotme, aes(x=tstat,group = group)) +
+  geom_histogram(
+    aes(fill = group, y = ..ncount..), position = 'identity', alpha = 0.6
+  )
+
+
+mean(cz_sum$tstat[cz_sum$tstat > 2])
+
+plotme %>% 
+  group_by(group) %>% 
+  summarize(
+    mean(tstat)
+  )
+
+
+
+# scratch -----------------------------------------------------------------
+
+t = cz_sum$tstat
+# t = t[t>2]
+
+1-ecdf(t)(3)
+1-ecdf(t)(2.27)
+
+0.2 + 0.8 * 0.05
+
+1-ecdf(t)(4)
+
+
+## YZ boot back of envelope ----------------------------------------------------
+
+# sim
+n = 18*1000
+s = 100
+z = rnorm(n*s) %>% matrix(nrow = s)
+
+qnum = 0.99
+  
+q = apply(z, 1, function(x) quantile(x,qnum))
+mean(q)
+sd(q)
+
+# hand calc
+Ehat_q = qnorm(qnum) 
+Vhat_q = qnum*(1-qnum)/(n*(dnorm(Ehat_q)^2)) 
+sdhat_q = sqrt(Vhat_q) %>% print
+
+q_too_big = Ehat_q + 2*sdhat_q 
+
+q_too_big
+
+mean(q)
+Ehat_q
+
+sd(q)
+sdhat_q
+
+sqrt(1/n*10)
+
+qnorm(0.99)
+
+# Fisher Tippet Gnedenko approx
+# this is too big
+n = 18*1000
+mumax = qnorm(1-1/n)
+semax = qnorm(1-1/n*exp(-1)) - qnorm(1-1/n)
+
+
+(10.67-2.32)/0.1
