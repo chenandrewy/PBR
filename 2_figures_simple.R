ba023755972cd767b124f52575379ba5db44f96c
AlecErb <alec.erb@gmail.com>
2022-08-24 13:36:09 -0400

    Streamline data imports + remove reliance on import_cz in Shrinkage figure

--- 2_figures_simple.R
+++ 2_figures_simple.R
@@ -3,6 +3,9 @@
 # SETUP ====
 
 rm(list = ls())
+source('functions.r')
+tic = Sys.time()
+dir.create('../results/')
 
 ## Globals ====
 library(tidyverse)
@@ -13,9 +16,11 @@ library(nlme)
 library(stargazer)
 library(lubridate)
 library(latex2exp)
+library(Cairo)
+library(xtable) 
 
 
-# Graph defaults
+# graph defaults
 MATBLUE = rgb(0,0.4470,0.7410)
 MATRED = rgb(0.8500, 0.3250, 0.0980)
 MATYELLOW = rgb(0.9290, 0.6940, 0.1250)
@@ -30,18 +35,29 @@ groupdat = list(
   , linetype = c('dashed','solid')
 )
 
+theme_set(
+  theme_minimal() +
+    theme(
+      text = element_text(family = "Palatino Linotype.ttf")
+    )
+)
 
-SUBDIR = 'Full Sets OP'; FILENAME = 'PredictorPortsFull.csv'
 
-dir.create('../results/')
+
+
+# Import Data ====
+
+cz_all = fread("../data/PredictorPortsFull.csv")
+
+signaldoc = fread('../data/SignalDoc.csv')
+
+
 
 # Generate McLean-Pontiff bootstrapped mean returns figure ====
 # read in PredictorPortsFull.csv
-ret0 = fread('../data/PredictorPortsFull.csv') %>%                                           
-  filter(!is.na(ret), port == 'LS')
 
 # read in SignalDoc.csv and isolates to relevant date cols
-signaldoc = fread('../data/SignalDoc.csv') %>% 
+signaldoc_mp = signaldoc %>% 
   mutate(
     signalname = Acronym
     , pubdate = as.Date(paste0(Year, '-12-31'))
@@ -53,8 +69,9 @@ signaldoc = fread('../data/SignalDoc.csv') %>%
 
 
 # merge and find OOS returns
-ret1 = ret0 %>%                                                                 ## merging, on signalname, returns and date (sampend, sampstart, pubdate) data
-  left_join(signaldoc) %>% 
+ret1 = cz_all %>%                                         
+  filter(!is.na(ret), port == 'LS') %>%                                                           
+  left_join(signaldoc_mp) %>% 
   mutate(
     samptype = case_when(
       (date >= sampstart) & (date <= sampend) ~ 'in-samp'
@@ -70,7 +87,6 @@ ret1 = ret0 %>%                                                                 
 # clustered by month
 
 set.seed(6)
-
 nboot = 500
 
 # make wide dataset, use NA if not correct sample
@@ -136,9 +152,9 @@ bootdat = data.frame(
 bootdat %>% 
   ggplot(aes(x=pooled_mean_ret, fill=samptype)) +
   geom_histogram(alpha = 0.8
-    , position = 'identity'
-    , breaks = seq(0,1,0.025)
-    , aes(y=..density..)
+                 , position = 'identity'
+                 , breaks = seq(0,1,0.025)
+                 , aes(y=..density..)
   ) +
   theme_minimal(
     base_size = 15,
@@ -162,7 +178,7 @@ ggsave(
 
 ## alt figure --------------------------------------------------------------
 
-  
+
 bootdat %>% 
   filter(samptype == 'out-of-samp') %>% 
   ggplot(aes(x=mean_ret_scaled, fill=samptype)) +
@@ -197,7 +213,8 @@ bootdat %>%
     base_size = 15,
   ) + 
   theme(
-    text = element_text(size=30)
+    text = element_text(size=40, family = "Palatino Linotype"),
+    legend.title=element_text(size=40)
   ) + 
   scale_fill_manual(
     values = c(groupdat$color[2], 'grey'), name = "Sample Type"
@@ -210,29 +227,28 @@ bootdat %>%
 ggsave(
   "../results/MPrep_scaled.pdf",
   width = 12,
-  height = 8
+  height = 8,
+  device = cairo_pdf
 )
 
 
 # Generate R2 Replication Figure ----
 
 ## Import ====
-
-# cz
-doc =  fread("../data/SignalDoc.csv") %>% 
+signaldoc_r2 =  signaldoc %>% 
   rename(signalname = Acronym) %>% 
   select(-c(`Detailed Definition`, `Notes`))
 
 
-cz_all = fread(paste0("../data/",FILENAME)) %>% 
+cz_r2 = cz_all %>% 
   mutate(vint = 2022) %>% 
   rbind(
-    fread(paste0("../data/",FILENAME)) %>% 
+    cz_all %>% 
       mutate(vint = 2021)
   ) %>% 
   select(vint, signalname, port, date, ret) %>% 
   left_join(
-    doc %>% select(signalname, SampleStartYear, SampleEndYear)
+    signaldoc_r2 %>% select(signalname, SampleStartYear, SampleEndYear)
     , by = 'signalname'
   ) %>% 
   mutate(
@@ -240,8 +256,7 @@ cz_all = fread(paste0("../data/",FILENAME)) %>%
   ) 
 
 ## Performance Measures ====
-
-target_data = cz_all %>% 
+target_data = cz_r2 %>%
   filter(vint == 2022 & insamp & port == 'LS') %>% 
   mutate(yearm = year(date)*100 + month(date)) %>% 
   select(signalname, yearm, ret)
@@ -258,8 +273,8 @@ fit_raw = target_data[
     model = 'raw'
   )
 
-## Name Scatter cleaner ====
 
+## Name Scatter cleaner ====
 ablines = tibble(slope = 1, 
                  intercept = 0,
                  group = factor(x = c('45 degree line'),
@@ -267,7 +282,7 @@ ablines = tibble(slope = 1,
 
 
 # select comparable t-stats
-fit_OP = doc %>% 
+fit_OP = signaldoc_r2 %>% 
   mutate(
     tstat_OP = abs(as.numeric(`T-Stat`))
   ) %>% 
@@ -280,12 +295,12 @@ fit_OP = doc %>%
     , `Predictability in OP` != 'indirect'
   ) 
 
-## Prepare signal doc ====
 
+## Prepare signal doc ====
 catname = c('raw','factor or char adjusted','nonstandard lag')
 
 # select comparable t-stats
-fit_OP = doc %>% 
+fit_OP = signaldoc_r2 %>% 
   mutate(
     tstat_OP = abs(as.numeric(`T-Stat`))
   ) %>% 
@@ -316,8 +331,8 @@ fit_OP = doc %>%
     )
   )
 
-## Raw vs OP plot ====
 
+## Raw vs OP plot ====
 # merge 
 fitcomp = fit_raw %>% 
   filter(model == 'raw') %>% 
@@ -337,7 +352,7 @@ fitcomp %>%
   ) +
   theme(
     legend.position = c(.8, .25),
-    text = element_text(size=30)
+    text = element_text(size=30, family = "Palatino Linotype")
   ) +
   geom_abline(
     aes(slope = 1, intercept = 0)
@@ -357,7 +372,8 @@ fitcomp %>%
 ggsave(
   "../results/raw_op.pdf",
   width = 12,
-  height = 8
+  height = 8,
+  device = cairo_pdf
 )
 
 
@@ -366,28 +382,25 @@ ggsave(
 # Generate t too big figure ====
 
 ## settings ====
-
 # for nboot = 1000, takes 20 seconds.  
 # nboot = 10,000 takes 3 minutes.  This is about 20 million predictors
-library(xtable) 
-
 nboot = 10*1000
 ndatemin = 240
 tedge = c(seq(0,9, 1), 20)
 
-## Import ====
 
+## Import ====
 # All signals
-doc =  fread("../data/SignalDoc.csv") %>%
+signaldoc_ttb = signaldoc %>%
   rename(signalname = Acronym) %>%
   select(-c(`Detailed Definition`, `Notes`))
 
 
 # Chen-Zimmerman dataset
-cz_all = fread(paste0("../data/PredictorPortsFull.csv"))  %>% 
+cz_ttb = cz_all  %>% 
   select(signalname, port, date, ret) %>%
   left_join(
-    doc %>% select(signalname, SampleStartYear, SampleEndYear)
+    signaldoc_ttb %>% select(signalname, SampleStartYear, SampleEndYear)
     , by = 'signalname'
   ) %>%
   mutate(
@@ -395,7 +408,7 @@ cz_all = fread(paste0("../data/PredictorPortsFull.csv"))  %>%
   )
 
 # Performance Measures 
-target_data = cz_all %>% 
+target_data = cz_ttb %>% 
   filter(insamp & port == 'LS') %>% 
   mutate(yearm = year(date)*100 + month(date)) %>% 
   select(signalname, yearm, ret)
@@ -412,10 +425,10 @@ fit_all = target_data[
     model = 'raw'
   )
 
-## Bootstrap ====
 
+## bootstrap ====
 # residuals
-e = cz_all %>% 
+cz_ttb = cz_ttb %>% 
   filter(insamp, port == 'LS') %>% 
   group_by(signalname) %>% 
   mutate(
@@ -429,12 +442,11 @@ e = cz_all %>%
 # bootstrap
 # fast binding based on https://stackoverflow.com/questions/19697700/how-to-speed-up-rbind
 boot = data.table()
-
 boot_once = function(){
-  dateselect = sample(1:dim(e)[1], ndatemax, replace = T)
+  dateselect = sample(1:dim(cz_ttb)[1], ndatemax, replace = T)
   
   # draw returns, clustered by month
-  eboot = e[dateselect, ]
+  eboot = cz_ttb[dateselect, ]
   
   # summarize each predictor
   ebar = apply(eboot, 2, mean, na.rm=T)
@@ -451,15 +463,13 @@ boot_once = function(){
   return(temp)
 }
 
-
 # bootstrap!!
-ndatemax = dim(e)[1]
+ndatemax = dim(cz_ttb)[1]
 tic = Sys.time()
 set.seed(1057)
 bootdat = rbindlist(lapply(1:nboot, function(x) boot_once()))
 toc = Sys.time()
 toc - tic
-
 
 # summarize across bootstraps
 bootsum = bootdat %>% 
@@ -470,7 +480,6 @@ bootsum = bootdat %>%
 
 
 ## Make Table ====
-
 # empirical cdf
 pemp = ecdf(fit_all$tstat)
 
@@ -495,41 +504,33 @@ tab_too_big = data.frame(
   mutate(
     emp_to_norm = prob_emp / prob_norm
     , N_hack_norm = N_emp/prob_norm
-  )
-
-
-tab_too_big = tab_too_big %>% 
+  ) %>% 
   mutate(t_bound = paste(t_left, " - ", t_right), .before=t_left) %>%
-  mutate(across(-t_bound, round, 2)) %>%
+  mutate(across(-t_bound, round, 6)) %>%
   as_tibble() %>%
   mutate_all(as.character())
 
-tab_too_big
-# mydf %>% mutate_at(vars(-vch1), funs(round(., 1)))
-# mydf %>% mutate(across(starts_with("vnum"), round, 1))
 
 tab_too_big_wide = tab_too_big %>% t()
 
-
-tab_too_big_wide
+# make table with Stargazer and export to LaTeX
 stargazer(tab_too_big_wide, type = "latex", title = "Results", align=TRUE)
-
 print(xtable(tab_too_big_wide, type = "latex"), file = "../results/tab-too-big.tex")
+
 
 
 
 # Correlation Figures ------------------------------------------------------
 
 # import data
-doc =  fread("../data/SignalDoc.csv") %>%
+signaldoc_corr =  signaldoc %>%
   rename(signalname = Acronym) %>%
   select(-c(`Detailed Definition`, `Notes`))
 
-
-cz_all = fread(paste0("../data/PredictorPortsFull.csv"))  %>% 
+cz_corr = cz_all  %>% 
   select(signalname, port, date, ret) %>%
   left_join(
-    doc %>% select(signalname, SampleStartYear, SampleEndYear)
+    signaldoc_corr %>% select(signalname, SampleStartYear, SampleEndYear)
     , by = 'signalname'
   ) %>%
   mutate(
@@ -537,34 +538,34 @@ cz_all = fread(paste0("../data/PredictorPortsFull.csv"))  %>%
   )
 
 # use full sample for most overlap
-retmat = cz_all %>% 
+retmat = cz_corr %>% 
   filter(port == 'LS') %>% 
   select(signalname, date, ret) %>% 
   pivot_wider(names_from = signalname, values_from = ret) %>%
   select(-date) %>% 
   as.matrix()
 
+
 ## correlation dist ====
 cormat = cor(retmat, use = 'pairwise.complete.obs')
-
 corlong = cormat[lower.tri(cormat)]
 
-
 ggplot(data.frame(cor = corlong), aes(x=cor)) +
-  geom_histogram(alpha = .8, fill="blue") +
+  geom_histogram(alpha = .8, fill=groupdat$color[2]) +
   theme_minimal(
     base_size = 15
   ) + 
   theme(
-    text = element_text(size=30)
+    text = element_text(size=40, family = "Palatino Linotype")
   ) + 
-  labs(x = 'Number of Principal Components'
-       ,y = 'Density')
+  labs(x = 'Pairwise Correlations'
+       ,y = 'Count')
 
 ggsave(
   "../results/correlation.pdf",
   width = 12,
-  height = 12
+  height = 12,
+  device = cairo_pdf
 )
 
 
@@ -572,7 +573,6 @@ ggsave(
 # some eigenvalues are negative, since we use available case correlations
 # this is not a real correlation matrix
 # let's just ignore this, it's not worth doing a "nearest PSD"
-
 eigval = eigen(cormat)$values
 pct_explained = cumsum(eigval/sum(eigval))*100
 
@@ -589,31 +589,164 @@ ggplot(plotme, aes(color="blue", x=Num_PC, y = pct_explained)) + geom_line() +
     base_size = 15
   ) + 
   theme(
-    text = element_text(size=30)
+    text = element_text(size=40, family = "Palatino Linotype")
   ) + 
-  labs(x="Number of Pairwise Correlations", y="% Varaince Explained") +
-  scale_color_manual(values=groupdat$color) +
+  labs(x="Number of Principal Components", y="% Varaince Explained") +
+  scale_color_manual(values=groupdat$color[2]) +
   theme(legend.position="none")
-
 
 ggsave(
   "../results/PCA.pdf",
   width = 12,
-  height = 12
+  height = 12,
+  device = cairo_pdf
 )
 
 
+# Filling the Gap ====
+
+## environment -------------------------------------------------------------
+
+# read returns
+cz_gap = cz_all %>%                                           
+  filter(!is.na(ret), port == 'LS')
+
+# read in SignalDoc.csv and isolates to relevant date cols
+signaldoc_gap = signaldoc %>% 
+  mutate(
+    signalname = Acronym
+    , pubdate = as.Date(paste0(Year, '-12-31'))
+    , sampend = as.Date(paste0(SampleEndYear, '-12-31'))
+    , sampstart = as.Date(paste0(SampleStartYear, '-01-01'))
+  ) %>% 
+  arrange(signalname) %>% 
+  select(signalname, pubdate, sampend, sampstart)
+
+
+# merge 
+ret1 = cz_gap %>%                                                                
+  left_join(signaldoc_gap) %>% 
+  mutate(
+    samptype = case_when(
+      (date >= sampstart) & (date <= sampend) ~ 'in-samp'
+      , (date > sampend) & (date <= pubdate) ~ 'out-of-samp'
+      , (date > pubdate) ~ 'post-pub'
+      , TRUE ~ NA_character_
+    )
+  ) %>% 
+  filter(!is.na(samptype))
+
+# find empirical t-stats
+t_emp = ret1 %>% filter(samptype == 'in-samp', port == 'LS') %>% 
+  group_by(signalname) %>% 
+  summarize(
+    tstat = mean(ret)/sd(ret)*sqrt(dplyr::n())
+  )  %>% 
+  pull(tstat)
 
 
 
-# Structural Figures ----
-# Based off of qml-pub-bias checkset script
+## make plotting data ----------------------------------------------------------------
 
-## environment ====
-rm(list = ls())
-source('functions.r')
-tic_all = Sys.time()
+# set up 
+edge = seq(0,20,0.5)
+t_left = edge[1:(length(edge)-1)]
+t_right = edge[2:length(edge)]
+mid = t_left + diff(edge)/2
 
+edge2  = seq(0,20,0.1)
+t_left2 = edge2[1:(length(edge2)-1)]
+t_right2 = edge2[2:length(edge2)]
+mid2 = t_left2 + diff(edge2)/2
+
+
+# empirical
+F_emp = ecdf(t_emp)
+dat_emp = data.frame(
+  t_mid = mid
+  , prob = (F_emp(t_right) - F_emp(t_left))
+  , group = 'emp'
+)
+
+# null
+rescalefac = mean(diff(edge))/mean(diff(edge2))
+dat_null =data.frame(
+  t_mid = mid2
+  , prob = (pnorm(t_right2) - pnorm(t_left2))/(1-pnorm(2))*rescalefac*(1-F_emp(2))
+  , group = 'null'
+) %>% 
+  mutate(prob = if_else(t_mid > 2, prob, 0))
+
+# fit
+sigfit = sqrt(1+3^2)
+dat_fit =data.frame(
+  t_mid = mid2
+  , prob = (pnorm(t_right2, sd = sigfit) - pnorm(t_left2, sd = sigfit))/
+    (1-pnorm(2, sd = sigfit))*rescalefac*(1-F_emp(2))
+  , group = 'fit'
+) %>% 
+  mutate(prob = if_else(t_mid > 2, prob, 0))
+
+# merge and factor group
+dat_all = rbind(dat_emp,dat_null,dat_fit) %>% 
+  mutate(
+    group = factor(group, levels = c('emp','null','fit'))
+  )
+
+
+## plot --------------------------------------------------------------------
+
+
+groupdat = tibble(
+  group = c('null', 'fit')
+  , color = c(MATRED, MATBLUE)
+  , labels = c(
+    TeX('Null ($\\sigma_\\mu=0$)')
+    , TeX('Fit ($\\sigma_\\mu=3$)')
+  )
+  , linetype = c('dashed','solid')
+)
+
+
+ggplot(dat_all %>%  filter(group == 'emp'), aes(x=t_mid,y=prob)) +
+  geom_bar(stat='identity',position='identity',alpha=0.6, aes(fill = group)) +
+  scale_fill_manual(
+    values = 'gray', labels = 'Published', name = NULL
+  ) +    
+  geom_line(
+    data = dat_all %>% filter(group != 'emp'), aes(color = group, linetype = group)
+  ) +
+  xlab('t-statistic') +
+  ylab('Frequency') +
+  scale_color_manual(
+    values = groupdat$color, labels = groupdat$labels, name = NULL
+  ) + 
+  scale_linetype_manual(
+    values = groupdat$linetype, labels = groupdat$labels, name = NULL
+  ) +
+  theme(
+    legend.position = c(75,75)/100
+    , legend.margin = margin(t = -15, r = 20, b = 0, l = 5)
+    , text = element_text(family = "Palatino Linotype")
+    
+  )  +
+  coord_cartesian(
+    xlim = c(0,10), ylim = c(0,0.25)
+  )  
+
+
+ggsave('../results/filling-the-gap.pdf', width = 12, height = 8, device = cairo_pdf)
+
+
+
+
+
+
+
+
+
+
+# Shrinkage Figure ----
 ## estimation settings  ====
 set.check = list(
   opt_method = 'two-stage' # 'two-stage' or 'crs-only' or 'pif-grid'
@@ -630,28 +763,24 @@ set.check = list(
   )  
 )
 
-## simulation settings ====
 
+## simulation settings ====
 piflist = seq(0.1,0.9,length.out = 3)
 rholist = seq(0.1,0.9,length.out = 3)
 nportlist = c(200)
 n_miniboot = 100
 
 
-## Point Estimate ----------------------------------------------------------
-
+## point estimate ----------------------------------------------------------
 tic = Sys.time()
+t_sample_mean = mean(t_emp[t_emp > 1.96])
 
-cz_filt_tabs = import_cz(dl=F) %>% filter(tabs >= 1.96) %>% pull(tabs)
-t_sample_mean = mean(cz_filt_tabs)
-
-# Build and solve truncated normal dist. for Lambda (x) [see Wikipedia - Truncated Nomrla]
+# build and solve truncated normal dist. for Lambda (x) [see Wikipedia - Truncated Nomrla]
 trunc_normal <- function(x) ((dnorm(2/x, mean = 0, sd = 1)) / (1-pnorm(2/x, mean =0, sd = 1))) * x - t_sample_mean
 lambda <- uniroot(trunc_normal, interval = c(.5, 10), tol = .00001)
 
-# Shrinkage formula (RAPS)
+# shrinkage formula (RAPS)
 shrink <- (1/(lambda$root**2))
-
 
 est.point = estimate(
   est.set = set.check
@@ -659,6 +788,7 @@ est.point = estimate(
   , par.guess = random_guess(set.check,1,1243)
   , print_level = 3
 )
+
 temp = make_stats_pub(cz_filt_tabs,est.point$par) # for now assume signs don't matter
 est.point$bias = temp$bias
 est.point$fdrloc = temp$fdr_tabs
@@ -668,20 +798,19 @@ est.point$negloglike %>% print()
 
 ## merge bias onto cz data with signalnames ====
 # this isn't pretty but it'll do for now
-
 temp = data.table(
   tabs = cz_filt_tabs, bias = est.point$bias
 )
 
-bias_dat = import_cz(dl = F) %>% 
+bias_dat = import_cz(dl = T) %>% 
   left_join(temp, by = 'tabs')
 
 # read in PredictorPortsFull.csv
-ret0 = fread('../data/PredictorPortsFull.csv') %>%                                           
+cz_bias = cz_all %>%                                           
   filter(!is.na(ret), port == 'LS')
 
 # read in SignalDoc.csv and isolates to relevant date cols
-signaldoc = fread('../data/SignalDoc.csv') %>% 
+signaldoc_bias = signaldoc %>% 
   mutate(
     signalname = Acronym
     , pubdate = as.Date(paste0(Year, '-12-31'))
@@ -692,8 +821,8 @@ signaldoc = fread('../data/SignalDoc.csv') %>%
   select(signalname, pubdate, sampend, sampstart)
 
 # merge 
-ret1 = ret0 %>%                                                                 ## merging, on signalname, returns and date (sampend, sampstart, pubdate) data
-  left_join(signaldoc) %>% 
+ret1 = cz_bias %>%                                                                 ## merging, on signalname, returns and date (sampend, sampstart, pubdate) data
+  left_join(signaldoc_bias) %>% 
   mutate(
     samptype = case_when(
       (date >= sampstart) & (date <= sampend) ~ 'insamp'
@@ -735,12 +864,13 @@ ggplot(plotme, aes(color = "blue", x=insamp, y=y, group = group)) +
     values = c('gray', 'blue'), name = "Sample Type"
   ) +
   labs(x = "In Sample Return", y = "Predicted Return")
-  
+
 
 ggsave(
   "../results/structural_figure.pdf",
   width = 12,
-  height = 12
+  height = 12,
+  device = cairo_pdf
 )
 
 
