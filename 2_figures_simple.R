bae9fbf69e0ab17f3c2bb9674f92b638869a8c6b
AlecErb <alec.erb@gmail.com>
2022-08-29 16:56:05 -0400

    initial commit for YZ dist. figure

--- 2_figures_simple.R
+++ 2_figures_simple.R
@@ -449,9 +449,6 @@ ggsave(
 
 
 # Filling the Gap ====
-# find empirical t-stats
-
-
 ## make plotting data ----------------------------------------------------------------
 # set up 
 edge = seq(0,20,0.5)
@@ -657,3 +654,103 @@ ggsave(
 )
 
 
+# Generate YZ Figure ====
+## import YZ ====
+temp = read_sas('../data-YZ/Yan_Zheng_RFS_Data.sas7bdat')
+
+yzsum = temp %>%
+  mutate(
+    signalname = paste(transformation, fsvariable, sep = '.')
+  ) %>%
+  transmute(
+    signalname, date = DATE, ret = 100*ddiff_ew
+  ) %>% 
+  filter(!is.na(ret)) %>% 
+  group_by(signalname) %>% 
+  summarize(
+    tstat = mean(ret)/sd(ret)*sqrt(dplyr::n()) %>% abs()
+  )
+
+# set up 
+edge = seq(0,20,0.5)
+t_left = edge[1:(length(edge)-1)]
+t_right = edge[2:length(edge)]
+mid = t_left + diff(edge)/2
+
+edge2  = seq(0,20,0.1)
+t_left2 = edge2[1:(length(edge2)-1)]
+t_right2 = edge2[2:length(edge2)]
+mid2 = t_left2 + diff(edge2)/2
+
+# empirical
+F_emp = ecdf(yzsum$tstat)
+dat_emp = data.frame(
+  t_mid = mid
+  , prob = (F_emp(t_right) - F_emp(t_left))
+  , group = 'emp'
+)
+
+# null
+rescalefac = mean(diff(edge))/mean(diff(edge2))
+dat_null =data.frame(
+  t_mid = mid2
+  , prob = (pnorm(t_right2) - pnorm(t_left2))/(1-pnorm(2))*rescalefac*(1-F_emp(2))
+  , group = 'null'
+) %>% 
+  mutate(prob = if_else(t_mid > 2, prob, 0))
+
+# fit
+sigfit = sqrt(1+3^2)
+dat_fit = data.frame(
+  t_mid = mid2
+  , prob = (pnorm(t_right2, sd = sigfit) - pnorm(t_left2, sd = sigfit))/
+    (1-pnorm(2, sd = sigfit))*rescalefac*(1-F_emp(2))
+  , group = 'fit'
+) %>% 
+  mutate(prob = if_else(t_mid > 2, prob, 0))
+
+# merge and factor group
+dat_all = rbind(dat_emp,dat_null,dat_fit) %>% 
+  mutate(
+    group = factor(group, levels = c('emp','null','fit'))
+  )
+
+
+## plot --------------------------------------------------------------------
+groupdat = tibble(
+  group = c('null', 'fit')
+  , color = c(MATRED, MATBLUE)
+  , labels = c(
+    TeX('Null ($\\sigma_\\mu=0$)')
+    , TeX('Fit ($\\sigma_\\mu=3$)')
+  )
+  , linetype = c('dashed','solid')
+)
+
+ggplot(dat_all %>%  filter(group == 'emp'), aes(x=t_mid,y=prob)) +
+  geom_bar(stat='identity',position='identity',alpha=0.6, aes(fill = group)) +
+  scale_fill_manual(
+    values = 'gray', labels = 'Published', name = NULL
+  ) +
+  
+  scale_color_manual(
+    values = groupdat$color, labels = groupdat$labels, name = NULL
+  ) +
+  
+  scale_linetype_manual(
+    values = groupdat$linetype, labels = groupdat$labels, name = NULL
+  ) +
+  
+  geom_line(
+    data = dat_all %>% filter(group != 'emp'), aes(color = group, linetype = group)
+  ) +
+  chen_theme +
+  theme(
+    legend.position = c(75,50)/100
+    # , legend.margin = margin(t = -15, r = 20, b = 0, l = 5)
+  )  +
+  xlab('t-statistic') +
+  ylab('Frequency') +
+  coord_cartesian(
+    xlim = c(0,8), ylim = c(0,0.2)
+  )  
