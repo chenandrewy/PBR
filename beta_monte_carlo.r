788b27c006d3ac330ae0d7bf51ac70b9baba41ce
Andrew Y. Chen <65506481+chenandrewy@users.noreply.github.com>
2022-08-31 15:01:18 -0400

    monte carlo dual panel drafted

--- beta_monte_carlo.r
+++ beta_monte_carlo.r
@@ -3,13 +3,12 @@
 
 library(data.table)
 
-n = 1e6
 
-# t-dist based on normal fit
-sigtheta = 3
-nu_g = 4
-sig_g = sqrt((nu_g-2)/nu_g)*sigtheta
-theta_t = rt(n,nu_g)*sig_g
+
+## simulate hlz ----------------------------------------------------------------
+
+n = 1e6
+set.seed(456)
 
 # hlz baseline
 v  = runif(n) > 0.444
@@ -26,13 +25,70 @@ dat = data.table(
     t = theta + Z, v = theta > 0, tabs = abs(t)
   )
 
-# fit
-datsum = dat %>% mutate(tgroup = ntile(t,100)) %>% 
+# fit shrinkage
+datsum = dat %>% mutate(tgroup = ntile(tabs,100)) %>% 
   group_by(tgroup) %>% 
   summarize(
-    Etheta = mean(theta), Et = mean(t)
+    tabs_left = min(tabs), tabs_right = max(tabs)
+    , Etabs = mean(tabs), Etheta = mean(theta), n = dplyr::n(), nfalse = sum(!v)
   )
 
+# fit "cdf"
+datsum = datsum %>% 
+  arrange(-Etabs) %>% 
+  mutate(
+    nfalse_cum = cumsum(nfalse)
+    , n_cum = cumsum(n)
+    , fdr_tabs_left = nfalse_cum / n_cum * 100
+  ) %>% 
+  arrange(Etabs)
+
+# find hurdles
+hurdle_05 = min(datsum$tabs_left[which(datsum$fdr_tabs_left < 5)])
+hurdle_01 = min(datsum$tabs_left[which(datsum$fdr_tabs_left < 1)])
+  
+
+## plot top panel --------------------------------------------------------------------
+
+# settings for both panels here
+xlimnum = c(0,6)
+
+ggplot(
+  dat[1:2000,]
+  , aes(x=tabs,y=theta)) +
+  geom_point(aes(group = v, color = v)) +
+  geom_vline(xintercept = 2) +
+  geom_vline(xintercept = hurdle_05) +
+  geom_vline(xintercept = hurdle_01) +
+  geom_line(
+    data = datsum, aes(x=Etabs, y=Etheta)
+  ) +
+  geom_abline(slope = 1) +
+  coord_cartesian(
+    xlim = xlimnum, ylim = c(-2,10)
+  ) +
+  theme(
+    legend.position = c(25,75)/100
+  )
+
+
+ggsave('../results/monte-carlo-top.pdf')
+
+
+## plot bottom panel -------------------------------------------------------
+
+ggplot(datsum, aes(x=tabs_left, y=fdr_tabs_left)) +
+  geom_line() +
+  geom_vline(xintercept = 2) +
+  geom_vline(xintercept = hurdle_05) +
+  geom_vline(xintercept = hurdle_01) +
+  coord_cartesian(
+    xlim = xlimnum, ylim = c(0, 20)
+  )
+
+ggsave('../results/monte-carlo-bottom.pdf')
+
+## numbers for text --------------------------------------------------------
 
 
 
@@ -51,28 +107,10 @@ dat %>%
     mean(t)
     , mean(theta)
     , mean(theta <= 0)    
-    , mean(theta) / mean(tabs)
+    , mean(theta) / mean(t)
   )
 
 
 # lm(theta ~ t, dat) %>% summary()
 
 
-set.seed(456)
-
-
-ggplot(
-  dat[1:2000,]
-  , aes(x=tabs,y=theta)) +
-  geom_point(aes(group = v, color = v)) +
-  geom_vline(xintercept = 2) +
-  geom_line(
-    data = datsum, aes(x=Et, y=Etheta)
-  ) +
-  geom_abline(slope = 1) +
-  coord_cartesian(
-    xlim = c(0,10), ylim = c(-5,12)
-  )
-
-
-ggsave('../results/monte-carlo.pdf')
